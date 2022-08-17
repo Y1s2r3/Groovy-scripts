c6c6f6e549f6b87c6ebca660ea019775f11987de
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-17 19:02:00 +0530

    Update Sonar.groovy

--- Sonar.groovy
+++ Sonar.groovy
@@ -1,6 +1,6 @@
 node {
   stage('Clone') {
-    git branch: "main" , url: "https://github.com/Y1s2r3/Groovy-scripts.git"
+    git branch: "main" , url: "https://github.com/Y1s2r3/Maven-demo.git"
   }
   stage('Build') {
     bat "cd initial && mvn clean install"
