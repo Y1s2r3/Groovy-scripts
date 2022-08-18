320bdd3205968635fb5832efe97830ee725b7658
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-18 11:48:27 +0530

    Update Sonar.groovy

--- Sonar.groovy
+++ Sonar.groovy
@@ -2,9 +2,6 @@ node {
   stage('Clone') {
     git branch: "main" , url: "https://github.com/Y1s2r3/Maven-demo.git"
   }
-  stage('Build') {
-    bat "cd initial && mvn clean install"
-  }
   stage('SonarCloud Analysis') {
     withSonarQubeEnv('SonarCloud')
     bat "mvn sonar:sonar"
