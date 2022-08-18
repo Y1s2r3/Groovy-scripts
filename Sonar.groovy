c5b78a3c9c1e681c001bf144f642f0019ceec81d
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-18 21:43:14 +0530

    Update Sonar.groovy

--- Sonar.groovy
+++ Sonar.groovy
@@ -5,10 +5,5 @@ node {
   stage('Build') {
     bat "cd initial && mvn clean install"
   }
-  stage('SonarCloud Analysis') {
-      withSonarQubeEnv(credentialsId: '4352577ae45e2fd0dafd6d24633ff7c77405f0bc', installationName: 'My SonarQube Server') {
-      sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.8.6:sonar'
-  }
-}
 }
   
