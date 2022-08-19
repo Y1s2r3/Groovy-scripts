23ec484baf92651a7bebf1c3876287bbe1992b67
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 09:34:51 +0530

    Update Sonar.groovy

--- Sonar.groovy
+++ Sonar.groovy
@@ -1,14 +1,17 @@
-node {
+ node {
   stage('Clone') {
-    git branch: "main" , url: "https://github.com/Y1s2r3/Maven-demo.git"
+  git "https://github.com/Y1s2r3/Maven-demo.git"
   }
   stage('Build') {
-    bat "cd initial && mvn clean install"
+    bat "mvn -Dmaven.test.failure.ignore=true clean package"
+    archiveArtifacts artifacts: 'target/*.jar', onlyIfSuccessful: true
   }
-  stage('SonarQube analysis') {
-    withSonarQubeEnv(credentialsId: 'a1893900d468b89894e5c7c0755b5a93574ac54d', installationName: 'Sonar-cloud') {
-      sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.8.6:sonar'
-    }
-  }
-}
-  
+  stage ('analysis') {
+    //bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar"
+    //junit 'target/surefire-reports/*.xml'
+    bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.8.6:sonar \
+    -Dsonar.projectKey=gs-maven1 \
+    -Dsonar.host.url=http://localhost:9000 \
+    -Dsonar.user=admin \
+    -Dsonar.login=sqp_e19f2b234afa50cc33d48e0436d19b3c134e3783"
+  } 
