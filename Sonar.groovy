cb1a0bb29570952be9119c323965723c41a8eed4
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 09:56:28 +0530

    Update Sonar.groovy

--- Sonar.groovy
+++ Sonar.groovy
@@ -6,12 +6,10 @@
     bat "cd initial && mvn clean install"
   }
   stage ('analysis') {
-    //bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar"
-    //junit 'target/surefire-reports/*.xml'
-    bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.8.6:sonar \
-    -Dsonar.projectKey=gs-maven1 \
-    -Dsonar.host.url=http://localhost:9000 \
-    -Dsonar.user=admin \
-    -Dsonar.login=sqp_e19f2b234afa50cc33d48e0436d19b3c134e3783"
+   bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar \
+  -Dsonar.projectKey=wertfg \
+  -Dsonar.host.url=http://localhost:9000 \
+   -Dsonar.user=admin \
+  -Dsonar.login=eb57ec8810b20a8b7ed1d9eebb39f80840e33009"
   } 
  }
